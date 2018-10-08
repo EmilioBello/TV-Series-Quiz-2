7b247de549da797c2c1de710f08b76b35b062741
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-10-08 20:43:15 +0200

    Added lombok and ADSerie, ADQuestion and ADVersion POJOs

---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/model/POJO/ADVersion.java
@@ -0,0 +1,19 @@
+package com.quiz.series.tv.tvseriesquiz.model.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Created by jose on 30/6/15.
+ */
+@Data
+@Builder
+@EqualsAndHashCode(callSuper=false)
+public class ADVersion{
+
+    private int code;
+
+    private String name;
+    private int version;
+}
