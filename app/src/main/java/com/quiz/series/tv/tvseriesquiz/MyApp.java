149d4f88fc08120d6df8b6058853b70ec040a072
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-10-17 16:31:23 +0200

    MyApp is executed when App begin and Realm is initialized.

---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/MyApp.java
@@ -0,0 +1,33 @@
+package com.quiz.series.tv.tvseriesquiz;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.quiz.series.tv.tvseriesquiz.model.realm.repository.ADRealm;
+
+
+public class MyApp extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //get Context
+        context = getApplicationContext();
+
+        //realm init
+        ADRealm realm = new ADRealm();
+        realm.init(context);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+    /*protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }*/
+}
