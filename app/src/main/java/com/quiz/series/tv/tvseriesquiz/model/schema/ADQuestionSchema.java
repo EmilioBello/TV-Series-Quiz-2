6fddb92ff4fd1f6822d5372dbe59a903d943369f
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-10-17 16:55:49 +0200

    Schemas have been created for not to access to database with parameters names directly

---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/model/schema/ADQuestionSchema.java
@@ -0,0 +1,12 @@
+package com.quiz.series.tv.tvseriesquiz.model.schema;
+
+public class ADQuestionSchema {
+
+    public static final String code = "code";
+    public static final String serieCode = "serieCode";
+
+    public static final String language = "language";
+
+    public static final String season = "season";
+    public static final String episode = "episode";
+}
---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/model/schema/ADVersionSchema.java
@@ -0,0 +1,7 @@
+package com.quiz.series.tv.tvseriesquiz.model.schema;
+
+public class ADVersionSchema {
+
+    public static final String code = "code";
+    public static final String name = "name";
+}
