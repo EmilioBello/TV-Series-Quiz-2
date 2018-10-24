543a4db63b55ed525fdea253a52328ee6a7f61c2
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-10-24 15:53:31 +0200

    General Interactor and interface to get Entities and interactor where you want to get all series.

---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/presenter/interactor/GetAllSeriesInteractor.java
@@ -0,0 +1,44 @@
+package com.quiz.series.tv.tvseriesquiz.presenter.interactor;
+
+import android.support.annotation.NonNull;
+
+import com.quiz.series.tv.tvseriesquiz.model.realm.entityDAO.ADSerieDAO;
+import com.quiz.series.tv.tvseriesquiz.model.schema.ADSerieSchema;
+import com.quiz.series.tv.tvseriesquiz.utils.executor.MainThread;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import io.realm.Realm;
+import io.realm.RealmModel;
+import io.realm.RealmQuery;
+
+public class GetAllSeriesInteractor<In, Out> extends GetEntitiesInteractor<In, Out>{
+
+    public GetAllSeriesInteractor(@NonNull final Executor executor, @NonNull final MainThread mainThread, @NonNull final Class<In> typeIn, @NonNull final Class<Out> typeOut) {
+        super(executor, mainThread, typeIn, typeOut);
+    }
+
+    @Override
+    public String getOrder() {
+        return ADSerieSchema.code;
+    }
+
+    @Override
+    public RealmQuery<RealmModel> buildQuery() {
+        RealmQuery<RealmModel> query;
+        Realm realm = Realm.getDefaultInstance();
+
+        final Class type = ADSerieDAO.class;
+
+        query = realm.where(type);
+        query.equalTo(ADSerieSchema.active, true);
+
+        return query;
+    }
+
+    @Override
+    public List<In> processResult(List<In> entities) {
+        return entities;
+    }
+}
---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/presenter/interactor/GetEntitiesInterface.java
@@ -0,0 +1,18 @@
+package com.quiz.series.tv.tvseriesquiz.presenter.interactor;
+
+import java.util.List;
+
+import io.realm.RealmModel;
+import io.realm.RealmQuery;
+
+public interface GetEntitiesInterface<In> {
+    void execute(Callback<In> callback);
+    String getOrder();
+    RealmQuery<RealmModel> buildQuery();
+    List<In> processResult(List<In> entities);
+
+    interface Callback<In> {
+        void onSuccess(final List<In> entities);
+        void onConnectionError();
+    }
+}
