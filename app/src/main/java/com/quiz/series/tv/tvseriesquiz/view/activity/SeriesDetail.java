6ef1cc1a3215788e6a8b73cd38e3cd94ff8484f6
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-11-08 17:35:43 +0100

    Created Activty for details of series.

---
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/view/activity/SeriesDetail.java
@@ -0,0 +1,15 @@
+package com.quiz.series.tv.tvseriesquiz.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.quiz.series.tv.tvseriesquiz.R;
+
+public class SeriesDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_series_detail);
+    }
+}
