fb8874b4c35b6420b60b12979219e00787ba8d90
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-11-13 12:24:22 +0100

    Added click listener ViewModel that have been clicked and view of the cardView clicked.

--- app/src/main/java/com/quiz/series/tv/tvseriesquiz/view/adapter/CommonAdapterListener.java
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/view/adapter/CommonAdapterListener.java
@@ -1,7 +1,8 @@
 package com.quiz.series.tv.tvseriesquiz.view.adapter;
 
+import android.support.annotation.NonNull;
 import android.view.View;
 
 public interface CommonAdapterListener<ViewModel> {
-    void getItem(ViewModel viewModel);
+    void getItem(@NonNull ViewModel viewModel, View view);
 }
