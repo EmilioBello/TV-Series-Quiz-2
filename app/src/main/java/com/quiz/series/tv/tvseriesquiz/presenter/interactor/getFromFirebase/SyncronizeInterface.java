11eef1927f0cc0876fb948ef554c5b626eb3c430
DESKTOP-C9CL18D\Emilio <emilio.bellovillanueva@gmail.com>
2018-11-21 18:35:51 +0100

    Removed my name and fate when this code was created

--- app/src/main/java/com/quiz/series/tv/tvseriesquiz/presenter/interactor/getFromFirebase/SyncronizeInterface.java
+++ app/src/main/java/com/quiz/series/tv/tvseriesquiz/presenter/interactor/getFromFirebase/SyncronizeInterface.java
@@ -2,10 +2,6 @@ package com.quiz.series.tv.tvseriesquiz.presenter.interactor.getFromFirebase;
 
 import android.support.annotation.NonNull;
 
-/**
- * Created by Emilio on 30/06/2016.
- */
-
 public interface SyncronizeInterface {
 
     interface Callback {
